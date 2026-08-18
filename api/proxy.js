bf1b7f380910f5505587c382d58376c79c0034e2
andresabel111-ux <andresabel111@gmail.com>
2026-08-17 22:57:03 -0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -3,13 +3,13 @@ module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
-  // URL principal del CRM (getClientes, addVenta, etc.)
   const GAS_MAIN = 'https://script.google.com/macros/s/AKfycbwg15wVGvEp98Aq1S0OnafmISuSz_fqpXDmq9q8gTcIJyL-tUafIHof99ZntkLhYVGBTg/exec';
+  const GAS_UPDATE = 'https://script.google.com/macros/s/AKfycbzLiT3KhhNTeLVztUuL_vIW__YMMa4y3y0a0WdJyauZENbU-XtTHthG_ILw-OOUAEQfew/exec';
 
-  // URL secundaria solo para updateCliente (observaciones y recordatorio)
-      const GAS_UPDATE = 'https://script.google.com/macros/s/AKfycbx1sHRzd4bXrV8OHGwy1cox1Q_KG2ljyJxok4k6zmDtpMyOeFDltSugbKEOiqf-xQhX/exec';
   const accion = (req.query.accion || '').toString();
-  const GAS = accion === 'updateCliente' ? GAS_UPDATE : GAS_MAIN;
+  const ESCRITURAS = ['addVenta', 'addCliente', 'updateContacto', 'updateCliente'];
+  const GAS = ESCRITURAS.includes(accion) ? GAS_UPDATE : GAS_MAIN;
+
   const params = new URLSearchParams(req.query).toString();
 
   try {
